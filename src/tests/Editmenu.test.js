1e198a085d8b35a54acbf826d036045b9a694858
Jonny Cheng <jcheng02@mica.edu>
2018-08-15 12:26:01 -0400

    jest enzyme npm reinstall seems to work now

--- src/tests/Editmenu.test.js
+++ src/tests/Editmenu.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import EditMenu from '../components/moodboard/EditMenu';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<EditMenu />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
