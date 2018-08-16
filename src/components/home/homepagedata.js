bfbbfc20cf7aaa5032b4307a690debc4f3bb81c5
Jonny Cheng <jcheng02@mica.edu>
2018-08-16 12:34:03 -0400

    basic reducer test

---
+++ src/components/home/homepagedata.js
@@ -0,0 +1,75 @@
+//these are used for the homepage upon initialization
+export const initialImages={
+    0: {
+     id:0,
+     imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333531/homepage/jordan.jpg',
+     position: [170,-50],
+     dimensions:[93,143],
+  
+    },
+    1:{
+     id:1,
+     imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333558/homepage/board.gif',
+     position: [596,139],
+     dimensions:[130,130],
+  
+    },
+    2:{
+      id:2,
+      imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333554/homepage/puppyjack.gif',
+      position:[-32,384],
+      dimensions:[100,100],
+    },
+    3:{
+      id:3,
+      imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333573/homepage/fruits.png',
+      position:[361,-36],
+      dimensions:[200,117],
+    },
+    4:{
+      id:4,
+      imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333573/homepage/poster.png',
+      position: [-60,130],
+      dimensions:[120,173],
+   
+     },
+     5:{
+       id:5,
+       imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333569/homepage/pcl.jpg',
+       position:[560,363],
+       dimensions:[130,130]
+     },
+     6:{
+       id:6,
+       imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333518/homepage/bed.png',
+       position:[100,130],
+       dimensions:[80,74]
+     },
+     7:{
+      id:7,
+      imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333573/homepage/angels.png',
+      position: [150,357],
+      dimensions:[101,104]
+   
+     },
+     8:{
+       id:8,
+       imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333581/homepage/kleenex.png',
+       position:[324,351],
+       dimensions:[150,202]
+     },
+     9:{
+       id:9,
+       imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333556/homepage/hi.gif',
+       position:[279,173],
+       dimensions:[30,30]
+     },
+      10:{
+        id:10,
+        imageurl:'https://res.cloudinary.com/moodimagescloud/image/upload/v1534333624/homepage/homehappy.jpg',
+        position:[510,105],
+        dimensions:[30,37]
+      }
+  }
+  
+  export const initialIds=[0,1,2,3,4,5,6,7,8,9,10];
